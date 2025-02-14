d846da1f33879a069b364b1ec43cd480fd69c8ab
shinebyul <qufdl8382@gmail.com>
2025-02-15 02:45:07 +0900

    consumer 로직 변경

--- CDC_payload/src/main/java/com/example/cdc_payload/global/infra/kafka/in/ChangeLogConsumer.java
+++ CDC_payload/src/main/java/com/example/cdc_payload/global/infra/kafka/in/ChangeLogConsumer.java
@@ -23,8 +23,10 @@ import com.example.cdc_payload.global.infra.kafka.out.NewPayloadData;
 import com.example.cdc_payload.global.infra.kafka.out.PayloadLogProducer;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.common.quota.ClientQuotaAlteration.Op;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
@@ -42,6 +44,110 @@ public class ChangeLogConsumer {
 
     @KafkaListener(topics = "change_data_log", groupId = "change_log_group")
     public void newCaptureEvent(Map<String, Object> event){
+
+        String tableName = event.get("TABLE_NAME").toString();
+        String operation = event.get("OPERATION").toString();
+        String rowId = event.get("ROW_ID").toString();
+        String logToString = "";
+
+        log.info(">>> 이벤트 수신 ...");
+        log.info(">>> TABLE_NAME : "+tableName);
+        log.info(">>> OPERATION : "+operation);
+        log.info(">>> ROWID : "+rowId);
+
+        if(operation.equals("DELETE")){
+            logToString = event.get("SQL_REDO").toString().split("'")[1];
+        }
+        else{
+            if(tableName.equals("COMMENTS")){
+                Optional<Comments> optional = commentsRepository.findByRowId(rowId);
+                if(optional.isPresent()){
+                    logToString = optional.get().logToString();
+                }else{
+                    log.info("존재하지 않는 ROW : "+rowId);
+                }
+            }else if(tableName.equals("EMOJI")){
+                Optional<Emoji> optional = emojiRepository.findByRowId(rowId);
+                if(optional.isPresent()){
+                    logToString = optional.get().logToString();
+                }else{
+                    log.info("존재하지 않는 ROW : "+rowId);
+                }
+            }else if(tableName.equals("INTERACTION")){
+                Optional<Interaction> optional = interactionRepository.findByRowId(rowId);
+                if(optional.isPresent()){
+                    logToString = optional.get().logToString();
+                }else{
+                    log.info("존재하지 않는 ROW : "+rowId);
+                }
+            }else if(tableName.equals("POST")){
+                Optional<Post> optional = postRepository.findByRowId(rowId);
+                if(optional.isPresent()){
+                    logToString = optional.get().logToString();
+                }else{
+                    log.info("존재하지 않는 ROW : "+rowId);
+                }
+            }else if(tableName.equals("ROLE")){
+                Optional<Role> optional = roleRepository.findByRowId(rowId);
+                if(optional.isPresent()){
+                    logToString = optional.get().logToString();
+                }else{
+                    log.info("존재하지 않는 ROW : "+rowId);
+                }
+            }else if(tableName.equals("USERS")){
+                Optional<Users> optional = userRepository.findByRowId(rowId);
+                if(optional.isPresent()){
+                    logToString = optional.get().logToString();
+                }else{
+                    log.info("존재하지 않는 ROW : "+rowId);
+                }
+            }
+        }
+
+
+        if(logToString.length() > 0){
+            payloadLogProducer.sendNewPayloadLogCaptureMessage(
+                    NewPayloadData.builder()
+                            .operation(operation)
+                            .tableName(tableName)
+                            .log(logToString)
+                            .build());
+        }
+    }
+
+    //        else{
+    //            // 테이블별 Repository 매핑
+    //            Optional<?> entityOptional = getEntityByRowId(tableName, rowId);
+    //
+    //            entityOptional.ifPresent(entity ->
+    //                    sendPayloadLog(operation, tableName, ((Loggable) entity).logToString())
+    //            );
+    //        }
+
+//    private Optional<?> getEntityByRowId(String tableName, String rowId) {
+//        return switch (tableName) {
+//            case "COMMENTS" -> commentsRepository.findByRowId(rowId);
+//            case "EMOJI" -> emojiRepository.findByRowId(rowId);
+//            case "INTERACTION" -> interactionRepository.findByRowId(rowId);
+//            case "POST" -> postRepository.findByRowId(rowId);
+//            case "ROLE" -> roleRepository.findByRowId(rowId);
+//            case "USERS" -> userRepository.findByRowId(rowId);
+//            default -> {
+//                log.warn("지원되지 않는 TABLE_NAME: " + tableName);
+//                yield Optional.empty();
+//            }
+//        };
+//    }
+}
+
+
+
+//        else if(operation.equals("INSERT")){
+//            logToString = event.get("SQL_REDO").toString().split("values \\(")[1]; //'43690','10','6','8');
+//            logToString = logToString.replace("'","").replace(")","").replace(";","");
+//        }
+
+
 
 //        {"ROW_ID":"AAATNPAAHAAAA7MABe",
 //                "RS_ID":" 0x00002c.000112ad.0010 ",
@@ -69,54 +175,4 @@ public class ChangeLogConsumer {
 //                "XIDSLT":18,
 //                "SQL_REDO":"delete from "C##DEEP"."INTERACTION" where "IDX" = '43690' and "COMMENT_IDX" = '10' and "EMOJI_IDX" = '6' and "USER_IDX" = '8' and ROWID = 'AAATNPAAHAAABODADq';"}
 
-        // delete log 는 idx만 String으로 ex) "1"
-        // insert log 는 values뒤에 스트링 그대로
-        // update log 는 값 다 가져와서 스트링으로 만들기
-
-
-
-
-        String tableName = event.get("TABLE_NAME").toString();
-        String operation = event.get("OPERATION").toString();
-        String logToString = "";
-
-        log.info(">>> 이벤트 수신 ...");
-        log.info(">>> TABLE_NAME : "+tableName);
-        log.info(">>> OPERATION : "+operation);
-
-        if(operation.equals("DELETE")){
-            logToString = event.get("SQL_REDO").toString().split("'")[1];
-        }else if(operation.equals("INSERT")){
-            logToString = event.get("SQL_REDO").toString().split("values \\(")[1]; //'43690','10','6','8');
-            logToString = logToString.replace("'","").replace(")","").replace(";","");
-        }else{
-            if(tableName.equals("COMMENTS")){
-                Comments comments = commentsRepository.findByRowId(event.get("ROW_ID").toString());
-                logToString = comments.logToString();
-            }else if(tableName.equals("EMOJI")){
-                Emoji emoji = emojiRepository.findByRowId(event.get("ROW_ID").toString());
-                logToString = emoji.logToString();
-            }else if(tableName.equals("INTERACTION")){
-                Interaction interaction = interactionRepository.findByRowId(event.get("ROW_ID").toString());
-                logToString = interaction.logToString();
-            }else if(tableName.equals("POST")){
-                Post post = postRepository.findByRowId(event.get("ROW_ID").toString());
-                logToString = post.logToString();
-            }else if(tableName.equals("ROLE")){
-                Role role = roleRepository.findByRowId(event.get("ROW_ID").toString());
-                logToString = role.logToString();
-            }else if(tableName.equals("USERS")){
-                Users users = userRepository.findByRowId(event.get("ROW_ID").toString());
-                logToString = users.logToString();
-            }
-        }
-
-        payloadLogProducer.sendNewPayloadLogCaptureMessage(
-                NewPayloadData.builder()
-                        .operation(operation)
-                        .tableName(tableName)
-                        .log(logToString)
-                        .build());
-    }
-}
 
