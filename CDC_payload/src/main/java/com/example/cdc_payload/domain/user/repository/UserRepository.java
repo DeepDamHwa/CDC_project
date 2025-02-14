adb286a537ace46d109b1e119e687456ab33691b
shinebyul <qufdl8382@gmail.com>
2025-02-15 02:42:41 +0900

    user-findbyrowid 반환 형식 optinal로 변경

--- CDC_payload/src/main/java/com/example/cdc_payload/domain/user/repository/UserRepository.java
+++ CDC_payload/src/main/java/com/example/cdc_payload/domain/user/repository/UserRepository.java
@@ -2,11 +2,13 @@ package com.example.cdc_payload.domain.user.repository;
 
 import com.example.cdc_payload.domain.interaction.model.Interaction;
 import com.example.cdc_payload.domain.user.model.Users;
+import java.util.Optional;
+import org.apache.kafka.common.quota.ClientQuotaAlteration.Op;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<Users, Long> {
     @Query(value = "SELECT * FROM users WHERE \"ROWID\" = :rowid", nativeQuery = true)
-    Users findByRowId(@Param("rowid") String rowid);
+    Optional<Users> findByRowId(@Param("rowid") String rowid);
 }
